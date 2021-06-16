9669e2bb37341737be091abcf0b738c1a56da5c7
koki kanasaka <k.koki980701@gmail.com>
2021-06-16 15:58:59 +0900

    コントローラーにリーダー以外編集できない機能を実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user.owner?(@team)
+    redirect_to team_path
+    end
+  end
 
   def create
     @team = Team.new(team_params)
